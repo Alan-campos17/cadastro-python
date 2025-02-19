df01879ef9d4bd4ca2b7174ac79cb194d837dc35
Alan-campos17 <149601081+Alan-campos17@users.noreply.github.com>
2025-02-19 17:16:04 +0000

    33

--- A.py
+++ A.py
@@ -4,9 +4,21 @@ def tela_cadastro():
     nome_cliente = input('Nome do cliente: ')
   
     endereco = input('Endereço da rua: ')
+
+    numero_telefone = input('numero telefone: ')
+
+    cpf = input('cadastro do cpf: ')
+
+    Email = input('cadastro email:')
+
+    data_nascimento = input('data de nascimento: ')
    
     print("\n=== Dados Cadastrados ===")
     print(f"Nome do Cliente: {nome_cliente}")
     print(f"Endereço: {endereco}")
-
+    print(f"numero telefone: {numero_telefone}")
+    print(f"cpf: {cpf}")
+    print(f"Email: {Email}")
+    print(f"data de nascimento{data_nascimento}")
+    
 tela_cadastro()
